e9907813d658e1ab6b2c0e63e8bde506d29767cb
AnnieMarkina <anniemurkina@gmail.com>
2020-04-25 18:12:23 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.7cd30ec96d8ca1b48636e3401092257a.js"
 );
 
 self.addEventListener('message', (event) => {
